ebaac9b23943ee9f55e866bd0cd7918899d9d1c1
cesart050892 <cesart050892@gmail.com>
2023-11-22 00:34:36 -0600

    include header

---
+++ app/Views/project/layouts/includes/header.php
@@ -0,0 +1,58 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="/adminlte/index2.html" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini">
+            <img src="<?= base_url("app/img/template/icono-blanco.png") ?>" alt="logo_mini" class="img-responsive" style="padding: 10px;">
+        </span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg">
+            <img src="<?= base_url("app/img/template/logo-blanco-lineal.png") ?>" alt="logo" class="img-responsive" style="padding:10px 0px">
+        </span>
+
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <!-- User Account: style can be found in dropdown.less -->
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <?php
+                        if (!empty(session("foto"))) {
+
+                            echo '<img src="' . session("foto") . '" class="user-image">';
+                        } else {
+                            echo '<img src="' . base_url("app/img/template/user/anonymous.png") . '" class="user-image">';
+                        }
+                        ?>
+                        <span class="hidden-xs"><?php echo session("nombre"); ?></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <!-- <li class="user-header">
+                        </li> -->
+                        <!-- Menu Body -->
+                        <li class="user-body">
+                            <div class="pull-right">
+                                <a href="salir" class="btn btn-default btn-flat">Salir</a>
+                            </div>
+                        </li>
+                        <!-- Menu Footer-->
+                        <!-- <li class="user-footer">
+                        </li> -->
+                    </ul>
+                </li>
+                <!-- Control Sidebar Toggle Button -->
+                <!-- <li>
+                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+                </li> -->
+            </ul>
+        </div>
+    </nav>
+</header>
